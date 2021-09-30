cfc476a67caca6984a39c0b36172744960a37e17
Impact-I <87244850+Impact-I@users.noreply.github.com>
2021-09-30 03:36:15 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -55,27 +55,27 @@ def networkLib():
     global libAppArm64,libAppArm,libAppX64,libAppX86,libios
     if len(libios[1]) != 0:
        try:
-        urlretrieve("https://github.com/Impact-I/reFlutter/releases/download/ios-"+libios[1]+"/Flutter", "Flutter")
+        urlretrieve("https://github.com/ptswarm/reFlutter/releases/download/ios-"+libios[1]+"/Flutter", "Flutter")
        except:
         libios='',''
     if len(libAppArm64[1]) != 0:
        try: 
-        urlretrieve("https://github.com/Impact-I/reFlutter/releases/download/android-"+libAppArm64[1]+"/libflutter_arm64.so", "libflutter_arm64.so")
+        urlretrieve("https://github.com/ptswarm/reFlutter/releases/download/android-"+libAppArm64[1]+"/libflutter_arm64.so", "libflutter_arm64.so")
        except:
         libAppArm64='',''
     if len(libAppArm[1]) != 0:
        try:
-        urlretrieve("https://github.com/Impact-I/reFlutter/releases/download/android-"+libAppArm[1]+"/libflutter_arm.so", "libflutter_arm.so")
+        urlretrieve("https://github.com/ptswarm/reFlutter/releases/download/android-"+libAppArm[1]+"/libflutter_arm.so", "libflutter_arm.so")
        except:
         libAppArm='',''
     if len(libAppX64[1]) != 0:
        try:
-        urlretrieve("https://github.com/Impact-I/reFlutter/releases/download/android-"+libAppX64[1]+"/libflutter_x64.so", "libflutter_x64.so")
+        urlretrieve("https://github.com/ptswarm/reFlutter/releases/download/android-"+libAppX64[1]+"/libflutter_x64.so", "libflutter_x64.so")
        except:
         libAppX64='',''       
     if len(libAppX86[1]) != 0:
        try:
-        urlretrieve("https://github.com/Impact-I/reFlutter/releases/download/android-"+libAppX86[1]+"/libflutter_x86.so", "libflutter_x86.so")
+        urlretrieve("https://github.com/ptswarm/reFlutter/releases/download/android-"+libAppX86[1]+"/libflutter_x86.so", "libflutter_x86.so")
        except:
         libAppX86='',''
     patchLibrary()
