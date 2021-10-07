821949235f8be48b4c54ea55c9129c7b2e036ee3
Impact-I <87244850+Impact-I@users.noreply.github.com>
2021-10-07 17:59:02 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -135,6 +135,11 @@ def zipdir(path, ziph):
                 ziph.write(os.path.join(root, file), os.path.relpath(os.path.join(root.replace('release/',''), file),os.path.join(path, '..')),zipfile.ZIP_DEFLATED)
 
 def replaceLibFlutter():
+    resp = urlopen('https://raw.githubusercontent.com/ptswarm/reFlutter/main/enginehash.csv').read()
+    if libappHash not in resp:
+        shutil.rmtree('libappTmp')
+        print("\n Engine SnapshotHash: "+libappHash+"\n\n This engine is currently not supported.\n Most likely this flutter application uses the Debug version engine which you need to build manually using Docker at the moment.\n More details: https://github.com/ptswarm/reFlutter\n")
+        sys.exit()
     if len(sys.argv) < 3:
         inputIPBurp()
         networkLib()
