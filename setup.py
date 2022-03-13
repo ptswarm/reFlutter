87eb8680b32d48b23908449dcbe67d77e50d49db
Impact-I <87244850+Impact-I@users.noreply.github.com>
2022-03-13 15:26:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ CLASSIFIERS = [
 
 setuptools.setup(
     name='reflutter',
-    version='0.6.0',
+    version='0.6.1',
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='impact',
